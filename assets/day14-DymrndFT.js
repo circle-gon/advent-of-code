88a4035181dd15e8c96adeca2c64fc818c5232e9
circle-gon <97845741+circle-gon@users.noreply.github.com>
2025-06-14 02:13:38 +0000

    Deploying to gh-pages from @ circle-gon/advent-of-code@d466edf5e9517ef2ab8205067b86fa4005f9b7e3 🚀

---
+++ assets/day14-DymrndFT.js
@@ -0,0 +1,152 @@
+import{c as a}from"./emwasm-DmxTmHdw.js";import{m as r}from"./index-C_urV7hb.js";const u=`
+input = import js.raw(memory<u8>(1))
+reactions = memory<u8>(1)
+items = memory<u64>(1)
+items2 = memory<f64>(1)
+stack = memory<u8>(1)
+keys = memory<u32>(1)
+
+fn getKey(key: u32)(idx: u32) -> u32 {
+  // 1-byte keys
+  for (; idx < 256; idx++) {
+    if (keys[idx] == key) { return idx }
+    if (keys[idx] == 0) {
+      keys[idx] = key
+      return idx
+    }
+  }
+  unreachable()
+}
+
+fn parse()(idx: u32, outIdx: u32, tidx: u32, accum: u32, addr: u32, addr2: u32) {
+  while (input[idx] != 0) {
+    tidx = idx
+    while (input[tidx] != 62) { tidx++ }
+    tidx += 2
+    while (input[tidx] != 32) {
+      accum = 10 * accum + (input[tidx] - 48)
+      tidx++
+    }
+    tidx++
+    while (input[tidx] != 10 & input[tidx] != 0) {
+      addr = 27 * addr + (input[tidx] - 64)
+      tidx++
+    }
+
+    if (input[tidx] == 10) { tidx++ }
+
+    // each reaction has at most 10 components and 1 number to represent the output
+    // each component has two numbers (quantity and type), so there are 21 numbers total
+    addr = getKey(addr) * 21
+    reactions[addr] = accum
+    accum = 0
+    addr++
+
+    while (input[idx] != 62) {
+      while (input[idx] != 32) {
+        accum = 10 * accum + (input[idx] - 48)
+        idx++
+      }
+      idx++
+      while (input[idx] != 44 & input[idx] != 32) {
+        addr2 = 27 * addr2 + (input[idx] - 64)
+        idx++
+      }
+
+      reactions[addr] = accum
+      reactions[addr + 1] = getKey(addr2)
+      addr += 2
+      accum = 0
+      addr2 = 0
+      idx += 2
+    }
+
+    idx = tidx
+    addr = 0
+  }
+}
+
+fn computeReq(count: u64)(
+  height: u32, top: u32, amt: u64, addr: u32, req: u64, idx: u32, base: u64,
+  fuel: u32, ore: u32
+) -> u64 {
+  memory.clear(items)
+  fuel = getKey(133554) // FUEL
+  ore = getKey(11426) // ORE
+
+  stack[0] = fuel
+  items[2 * fuel] = count
+  items[2 * fuel + 1] = 0
+  height++
+  
+  while (height != 0) {
+    top = stack[height - 1]
+    height--
+    amt = items[top * 2] - items[top * 2 + 1]
+    if (sint(amt) <= 0 | top == ore) { continue }
+    addr = top * 21
+    base = i64(reactions[addr])
+    req = (amt + base - 1) / base
+    for (idx = 1; idx < 21; idx += 2) {
+      items[reactions[addr + idx + 1] * 2] += i64(reactions[addr + idx]) * req
+      stack[height] = reactions[addr + idx + 1]
+      height++
+    }
+    items[top * 2 + 1] += req * base
+  }
+  return items[ore * 2]
+}
+
+export fn part1()() -> u32 {
+  memory.clear(reactions)
+  parse()
+  return i32(computeReq(1))
+}
+
+export fn part2()(low: u64, high: u64, mid: u64) -> u32 {
+  memory.clear(reactions)
+  parse()
+ 
+  low = 1e18 / computeReq(1e6)
+  high = max()
+
+  while (low < high) {
+    mid = low + (high - low + 1) / 2
+    if (computeReq(mid) <= 1e12) {
+      low = mid
+    } else {
+      high = mid - 1 
+    }
+  }
+  return i32(low)
+}
+
+fn max()(
+  height: u32, top: u32, amt: f64, addr: u32, req: f64, idx: u32,
+  fuel: u32, ore: u32
+) -> u64 {
+  memory.clear(items2)
+  fuel = getKey(133554) // FUEL
+  ore = getKey(11426) // ORE
+
+  stack[0] = fuel
+  items2[fuel] = 1
+  height++
+  
+  while (height != 0) {
+    top = stack[height - 1]
+    height--
+    amt = items2[top]
+    if (amt <= 0 | top == ore) { continue }
+    addr = top * 21
+    req = amt / f64.convert_i32_u(reactions[addr])
+    for (idx = 1; idx < 21; idx += 2) {
+      items2[reactions[addr + idx + 1]] += f64.convert_i32_u(reactions[addr + idx]) * req
+      stack[height] = reactions[addr + idx + 1]
+      height++
+    }
+    items2[top] = 0
+  }
+  return i64.trunc_f64_u(generic.floor(1e12 / items2[ore]))
+}
+`,d=a(u,{},{});async function o(e){const{module:t,memory:i}=await d;return r(e,i),t.part1()}async function m(e){const{module:t,memory:i}=await d;return r(e,i),t.part2()}const s=[o,m];export{s as default};
---
+++ assets/day5-B7GFG2BG.js
@@ -0,0 +1,406 @@
+import{a as c,m as t}from"./index-C_urV7hb.js";const a=`
+(import "js" "raw" (memory $raw 1))
+(memory $order 1)
+(memory $reports 1)
+(memory $indexes 1)
+
+(func $parse 
+  (local $idx i32)
+  (local $str i32)
+  (local $num i32)
+  (local $out i32)
+  (local $mode i32)
+  
+  (loop $loop
+    (local.set $str
+      (i32.load8_u
+        (memory $raw)
+        (local.get $idx)
+      )
+    )
+    (i32.and
+      (i32.ge_u (local.get $str) (i32.const 48))
+      (i32.le_u (local.get $str) (i32.const 57))
+    )
+    (if
+      (then
+        (local.set $num
+          (i32.add
+            (i32.mul (local.get $num) (i32.const 10))
+            (i32.sub (local.get $str) (i32.const 48))
+          )
+        )
+      )
+      (else
+        (i32.eqz (local.get $num))
+        (if
+          (then
+            (local.set $mode (i32.const 1))
+            ;; Add a padding 0
+            (i32.store8
+              (memory $order)
+              (local.get $out)
+              (i32.const 0)
+            )
+            (local.set $out (i32.const 0))
+          )
+          (else
+            (i32.eqz (local.get $mode))
+            (if
+              (then
+                (i32.store8
+                  (memory $order)
+                  (local.get $out)
+                  (local.get $num)
+                )
+              )
+              (else
+                (i32.store8
+                  (memory $reports)
+                  (local.get $out)
+                  (local.get $num)
+                )
+                (i32.or
+                  (i32.eq (local.get $str) (i32.const 10)) ;; Newline
+                  (i32.eq (local.get $str) (i32.const 0)) ;; End of input
+                )
+                (if
+                  (then
+                    (local.set $out
+                      (i32.add
+                        (local.get $out)
+                        (i32.const 1)
+                      )
+                    )
+                    (i32.store8
+                      (memory $reports)
+                      (local.get $out)
+                      (i32.const -1) ;; End of update
+                    )
+                  )
+                )
+              )
+            )
+            (local.set $num (i32.const 0))
+            (local.set $out
+              (i32.add
+                (local.get $out)
+                (i32.const 1)
+              )
+            )
+          )
+        )
+      )
+    )
+    (i32.ne
+      (i32.load8_u
+        (memory $raw)
+        (local.get $idx)
+      )
+      (i32.const 0)
+    )
+    (local.set $idx
+      (i32.add (local.get $idx) (i32.const 1))
+    )
+    br_if $loop
+  )
+  ;; Add a padding zero
+  (i32.store8
+    (memory $reports)
+    (local.get $out)
+    (i32.const 0)
+  )
+)
+
+(func $works
+  (result i32)
+  (local $i1 i32)
+  (local $i2 i32)
+  (local $ord i32)
+  
+  (loop $loop
+    (local.set $i1
+      (i32.load8_u
+        (memory $order)
+        (local.get $ord)
+      )
+    )
+    (local.set $ord
+      (i32.add (local.get $ord) (i32.const 1))
+    )
+    (local.set $i2
+      (i32.load8_u
+        (memory $order)
+        (local.get $ord)
+      )
+    )
+    (local.set $ord
+      (i32.add (local.get $ord) (i32.const 1))
+    )
+  
+    ;; Cheat by reusing the local
+    (local.set $i1
+      (i32.load8_u
+        (memory $indexes)
+        (local.get $i1)
+      )
+    )
+    (local.set $i2
+      (i32.load8_u
+        (memory $indexes)
+        (local.get $i2)
+      )
+    )
+    (i32.and
+      (i32.ne (local.get $i1) (i32.const 0))
+      (i32.ne (local.get $i2) (i32.const 0))
+    )
+    (i32.ge_u (local.get $i1) (local.get $i2))
+    i32.and
+    (if
+      (then
+        local.get $ord
+        return
+      )
+    )
+
+    (i32.ne
+      (i32.load8_s
+        (memory $order)
+        (local.get $ord)
+      )
+      (i32.const 0)
+    )
+    br_if $loop
+  )
+  i32.const 0
+)
+
+(func $check
+  (result i32)
+  (local $idx i32)
+  (local $num i32)
+  (local $sum i32)
+  (local $count i32)
+  
+  (loop $loop
+    (local.set $num
+      (i32.load8_s
+        (memory $reports)
+        (local.get $idx)
+      )
+    )
+    
+    (i32.ne (local.get $num) (i32.const -1))
+    (if
+      (then
+        (local.set $count (i32.add (local.get $count) (i32.const 1)))
+        (i32.store8
+          (memory $indexes)
+          (local.get $num)
+          (local.get $count)
+        )
+      )
+      (else
+        (i32.eqz (call $works))
+        (if
+          (then
+            (i32.sub
+              (local.get $idx)
+              (i32.add
+                (i32.div_u
+                  (local.get $count)
+                  (i32.const 2)
+                )
+                (i32.const 1)
+              )
+            )
+            (i32.load8_u (memory $reports))
+            local.get $sum
+            i32.add
+            local.set $sum
+          )
+        )
+        ;; Clear all indexes (there's no way around it)
+        (memory.fill
+          (memory $indexes)
+          (i32.const 0)
+          (i32.const 0)
+          (i32.const 65536)
+        )
+        (local.set $count (i32.const 0))
+      )
+    )
+  
+    (local.set $idx
+      (i32.add (local.get $idx) (i32.const 1))
+    )
+    (i32.ne
+      (i32.load8_s
+        (memory $reports)
+        (local.get $idx)
+      )
+      (i32.const 0)
+    )
+    br_if $loop
+  )
+  local.get $sum
+)
+
+(func $check2
+  (result i32)
+  (local $idx i32)
+  (local $num i32)
+  (local $sum i32)
+  (local $count i32)
+  (local $ridx i32)
+  (local $num1 i32)
+  (local $temp1 i32)
+  (local $temp2 i32)
+  (local $test i32)
+  (local $idx2 i32)
+  (local $num2 i32)
+  
+  (loop $loop
+    (local.set $num
+      (i32.load8_s
+        (memory $reports)
+        (local.get $idx)
+      )
+    )
+    
+    (i32.ne (local.get $num) (i32.const -1))
+    (if
+      (then
+        (local.set $count (i32.add (local.get $count) (i32.const 1)))
+        (i32.store8
+          (memory $indexes)
+          (local.get $num)
+          (local.get $count)
+        )
+      )
+      (else
+        (local.set $ridx (call $works))
+        (i32.ne (local.get $ridx) (i32.const 0))
+        (if
+          (then
+            (loop $loop2
+              ;; Swap thing
+              (local.set $num1
+                (i32.load8_u
+                  (memory $order)
+                  (i32.sub
+                    (local.get $ridx)
+                    (i32.const 2)
+                  )
+                )
+              )
+              (local.set $num2
+                (i32.load8_u
+                  (memory $order)
+                  (i32.sub
+                    (local.get $ridx)
+                    (i32.const 1)
+                  )
+                )
+              )
+              (local.set $temp1
+                (i32.load8_u
+                  (memory $indexes)
+                  (local.get $num1)
+                )
+              )
+              (local.set $temp2
+                (i32.load8_u
+                  (memory $indexes)
+                  (local.get $num2)
+                )
+              )
+              (i32.store8
+                (memory $indexes)
+                (local.get $num1)
+                (local.get $temp2)
+              )
+              (i32.store8
+                (memory $indexes)
+                (local.get $num2)
+                (local.get $temp1)
+              )
+              (local.set $ridx (call $works))
+              (i32.ne (local.get $ridx) (i32.const 0))
+              br_if $loop2
+              ;; Find the original index
+              (local.set $test
+                (i32.add
+                  (i32.div_u
+                    (local.get $count)
+                    (i32.const 2)
+                  )
+                  (i32.const 1)
+                )
+              )
+              (loop $loop3
+                (local.set $num2
+                  (i32.load8_u
+                    (memory $indexes)
+                    (local.get $idx2)
+                  )
+                )
+                (i32.eq (local.get $test) (local.get $num2))
+                (if
+                  (then
+                    (local.set $sum
+                      (i32.add
+                        (local.get $sum)
+                        (local.get $idx2)
+                      )
+                    )
+                  )
+                  (else
+                    (local.set $idx2
+                      (i32.add (local.get $idx2) (i32.const 1))
+                    )
+                    br $loop3
+                  )
+                )
+              )
+              (local.set $idx2 (i32.const 0))
+            )
+          )
+        )
+        ;; Clear all indexes (there's no way around it)
+        (memory.fill
+          (memory $indexes)
+          (i32.const 0)
+          (i32.const 0)
+          (i32.const 65536)
+        )
+        (local.set $count (i32.const 0))
+      )
+    )
+  
+    (local.set $idx
+      (i32.add (local.get $idx) (i32.const 1))
+    )
+    (i32.ne
+      (i32.load8_s
+        (memory $reports)
+        (local.get $idx)
+      )
+      (i32.const 0)
+    )
+    br_if $loop
+  )
+  local.get $sum
+)
+
+(func (export "part1") (result i32)
+  call $parse
+  call $check
+)
+
+(func (export "part2") (result i32)
+  call $parse
+  call $check2
+)
+`,i=c(a,{});async function $(o){const{module:l,memory:e}=await i;return t(o,e),l.part1()}async function s(o){const{module:l,memory:e}=await i;return t(o,e),l.part2()}const r=[$,s];export{r as default};
